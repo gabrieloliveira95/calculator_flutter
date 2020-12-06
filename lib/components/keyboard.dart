b378dc43e4d90ce7812c62c5b57d8edf2177ea64
Gabriel Oliveira <gabrieloliveira95@live.com>
2020-12-06 11:55:04 -0300

    first commit

---
+++ lib/components/keyboard.dart
@@ -0,0 +1,54 @@
+import 'package:calculator/components/button.dart';
+import 'package:calculator/components/button_row.dart';
+import 'package:flutter/material.dart';
+
+class KeyBoard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 500,
+      child: Column(
+        children: [
+          ButtonRow(
+            buttons: [
+              Button(text: 'AC', big: true),
+              Button(text: '%'),
+              Button(text: '/'),
+            ],
+          ),
+          ButtonRow(
+            buttons: [
+              Button(text: '7'),
+              Button(text: '8'),
+              Button(text: '9'),
+              Button(text: 'x'),
+            ],
+          ),
+          ButtonRow(
+            buttons: [
+              Button(text: '4'),
+              Button(text: '5'),
+              Button(text: '6'),
+              Button(text: '-'),
+            ],
+          ),
+          ButtonRow(
+            buttons: [
+              Button(text: '1'),
+              Button(text: '2'),
+              Button(text: '3'),
+              Button(text: '+'),
+            ],
+          ),
+          ButtonRow(
+            buttons: [
+              Button(text: '0', big: true),
+              Button(text: ','),
+              Button(text: '='),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
